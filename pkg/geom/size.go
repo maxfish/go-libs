c0a35f1b672fcfa1844393698d6ec79df2fbd36f
Massimiliano Pesce <maxfish@users.noreply.github.com>
2021-04-03 19:45:37 +0200

    Update size.go

--- pkg/geom/size.go
+++ pkg/geom/size.go
@@ -9,17 +9,24 @@ type Size struct {
 	W, H int
 }
 
+func MaxSize(a,b Size) Size {
+	return Size{
+		W: math.MaxI(a.W, b.W),
+		H: math.MaxI(a.H, b.H),
+	}
+}
+
+func (s Size) Sub(t Size) Size {
+	return Size{
+		W: s.W - t.W,
+		H: s.H - t.H,
+	}
+}
+
 func (s Size) Scale(factor float32) Size {
 	return Size{
 		W: int(float32(s.W) * factor),
 		H: int(float32(s.H) * factor),
-	}
-}
-
-func (s Size) Union(other Size) Size {
-	return Size{
-		W: math.MaxI(s.W, other.W),
-		H: math.MaxI(s.H, other.H),
 	}
 }
 
