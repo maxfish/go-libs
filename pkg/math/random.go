da1dd5db5d099f6e542edf2b6aebd97035fd88ff
Massimiliano Pesce <maxfish@users.noreply.github.com>
2021-04-08 20:01:10 +0200

    Random

---
+++ pkg/math/random.go
@@ -0,0 +1,31 @@
+package math
+
+import "math/rand"
+
+type RandomGenerator struct {
+	generator *rand.Rand
+}
+
+func NewRandomGenerator(seed int64) *RandomGenerator {
+	r := &RandomGenerator{
+		generator: rand.New(rand.NewSource(seed)),
+	}
+	return r
+}
+
+func (g *RandomGenerator) NextInt() int {
+	return g.generator.Int()
+}
+
+func (g *RandomGenerator) NextFloat() float32 {
+	return g.generator.Float32()
+}
+
+func (g *RandomGenerator) Permutation(max int) []int {
+	return g.generator.Perm(max)
+}
+
+func (g *RandomGenerator) Event(chance int) bool {
+	return g.generator.Uint64()%100 < uint64(chance)
+}
+
