b2b1b9299c85ea6e7680bf6703b1adc9a9bd8862
Massimiliano Pesce <maxfish@users.noreply.github.com>
2021-04-10 18:16:23 +0200

    Added 1D perlin noise

---
+++ pkg/fmath/perlin_noise.go
@@ -0,0 +1,154 @@
+package fmath
+
+/**
+ * Code ported to Go from: https://github.com/SRombauts/SimplexNoise
+ * Copyright (c) 2014-2018 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+ * Distributed under the MIT License (MIT) (See accompanying file licenses/SimplexNoise.txt)
+ */
+
+type PerlinNoise struct {
+	// Parameters of Fractional Brownian Motion (fBm) : sum of N "octaves" of noise
+	frequency   float32 //< Frequency ("width") of the first octave of noise (default to 1.0)
+	amplitude   float32 //< Amplitude ("height") of the first octave of noise (default to 1.0)
+	lacunarity  float32 //< Lacunarity specifies the frequency multiplier between successive octaves (default to 2.0).
+	persistence float32 //< Persistence is the loss of amplitude between successive octaves (usually 1/lacunarity)
+}
+
+/**
+ * Constructor of to initialize a fractal noise summation
+ *
+ * @param[in] frequency    Frequency ("width") of the first octave of noise (default to 1.0)
+ * @param[in] amplitude    Amplitude ("height") of the first octave of noise (default to 1.0)
+ * @param[in] lacunarity   Lacunarity specifies the frequency multiplier between successive octaves (default to 2.0).
+ * @param[in] persistence  Persistence is the loss of amplitude between successive octaves (usually 1/lacunarity)
+ */
+func NewPerlinNoiseWithParameters(frequency, amplitude, lacunarity, persistence float32) *PerlinNoise {
+	return &PerlinNoise{
+		frequency:   frequency,
+		amplitude:   amplitude,
+		lacunarity:  lacunarity,
+		persistence: persistence,
+	}
+}
+
+func NewPerlinNoise() *PerlinNoise {
+	return NewPerlinNoiseWithParameters(1, 1, 2, 0.5)
+}
+
+/**
+ * 1D Perlin simplex noise
+ * @param[in] x float coordinate
+ * @return Noise value in the range[-1; 1], value of 0 on all integer coordinates.
+ */
+func (n *PerlinNoise) Noise1D(x float32, seed int32) float32 {
+	var n0, n1 float32 // Noise contributions from the two "corners"
+
+	// Corners coordinates (nearest integer values):
+	var i0 = fastfloor(x)
+	var i1 = i0 + 1
+	// Distances to corners (between 0 and 1):
+	var x0 = x - float32(i0)
+	var x1 = x0 - 1.0
+
+	// Calculate the contribution from the first corner
+	var t0 = 1.0 - x0*x0
+	t0 *= t0
+	n0 = t0 * t0 * grad(hash(i0+seed*7), x0)
+
+	// Calculate the contribution from the second corner
+	var t1 = 1.0 - x1*x1
+	t1 *= t1
+	n1 = t1 * t1 * grad(hash(i1+seed*7), x1)
+
+	// The maximum value of this noise is 8*(3/4)^4 = 2.53125
+	// A factor of 0.395 scales to fit exactly within [-1,1]
+	return 0.395 * (n0 + n1)
+}
+
+// TODO test if this is faster than the standard function
+// Computes the largest integer value not greater than the float one
+func fastfloor(fp float32) int32 {
+	var i = int32(fp)
+	if fp < float32(i) {
+		return i - 1
+	} else {
+		return i
+	}
+}
+
+/**
+ * Permutation table. This is just a random jumble of all numbers 0-255.
+ *
+ * This produce a repeatable pattern of 256, but Ken Perlin stated
+ * that it is not a problem for graphic texture as the noise features disappear
+ * at a distance far enough to be able to see a repeatable pattern of 256.
+ *
+ * This needs to be exactly the same for all instances on all platforms,
+ * so it's easiest to just keep it as static explicit data.
+ * This also removes the need for any initialisation of this class.
+ *
+ * Note that making this an uint32_t[] instead of a uint8_t[] might make the
+ * code run faster on platforms with a high penalty for unaligned single
+ * byte addressing. Intel x86 is generally single-byte-friendly, but
+ * some other CPUs are faster with 4-aligned reads.
+ * However, a char[] is smaller, which avoids cache trashing, and that
+ * is probably the most important aspect on most architectures.
+ * This array is accessed a *lot* by the noise functions.
+ * A vector-valued noise over 3D accesses it 96 times, and a
+ * float-valued 4D noise 64 times. We want this to fit in the cache!
+ */
+var perm = [256]byte{
+	151, 160, 137, 91, 90, 15,
+	131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
+	190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
+	88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166,
+	77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
+	102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196,
+	135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
+	5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
+	223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
+	129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228,
+	251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
+	49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
+	138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180,
+}
+
+/**
+ * Helper function to hash an integer using the above permutation table
+ *
+ *  This inline function costs around 1ns, and is called N+1 times for a noise of N dimension.
+ *
+ *  Using a real hash function would be better to improve the "repeatability of 256" of the above permutation table,
+ * but fast integer Hash functions uses more time and have bad random properties.
+ *
+ * @param[in] i Integer value to hash
+ *
+ * @return 8-bits hashed value
+ */
+func hash(i int32) int32 {
+	return int32(perm[byte(i)])
+}
+
+/**
+ * Helper function to compute gradients-dot-residual vectors (1D)
+ *
+ * @note that these generate gradients of more than unit length. To make
+ * a close match with the value range of classic Perlin noise, the final
+ * noise values need to be rescaled to fit nicely within [-1,1].
+ * (The simplex noise functions as such also have different scaling.)
+ * Note also that these noise functions are the most practical and useful
+ * signed version of Perlin noise.
+ *
+ * @param[in] hash  hash value
+ * @param[in] x     distance to the corner
+ *
+ * @return gradient value
+ */
+func grad(hash int32, x float32) float32 {
+	var h = hash & 0x0F           // Convert low 4 bits of hash code
+	var grad = 1.0 + float32(h&7) // Gradient value 1.0, 2.0, ..., 8.0
+	if (h & 8) != 0 {
+		grad = -grad // Set a random sign for the gradient
+	}
+	return grad * x // Multiply the gradient with the distance
+}
