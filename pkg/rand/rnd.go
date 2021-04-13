6e4e287d0d8e70bbd2b0c85cd70face9b0531c86
Massimiliano Pesce <maxfish@users.noreply.github.com>
2021-04-13 07:48:34 +0200

    Added interface for Rnd generators

---
+++ pkg/rand/rnd.go
@@ -0,0 +1,10 @@
+package rand
+
+type RndGenerator interface {
+	NextUint32() (result uint32)
+	NextUint32LessThan(n int) (result uint32)
+	NextUint32InRange(min, max int) (result uint32)
+	NextFloat32() (result float32)
+	Event(chance int) bool
+	Maybe() bool
+}
